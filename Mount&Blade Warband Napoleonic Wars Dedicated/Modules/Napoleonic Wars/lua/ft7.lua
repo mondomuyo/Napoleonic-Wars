46237103653718f4f49c908fae310f1389f2eaa3
Mondo Muyo <mawanqiangkf@163.com>
2021-04-24 20:13:00 +0800

    Add some colors to ft7 messages.

--- Mount&Blade Warband Napoleonic Wars Dedicated/Modules/Napoleonic Wars/lua/ft7.lua
+++ Mount&Blade Warband Napoleonic Wars Dedicated/Modules/Napoleonic Wars/lua/ft7.lua
@@ -11,7 +11,7 @@ local ft7_players = {}
 function checkDuelValid(player1, player2)
     if(player2 == nil) then
         if(ft7_players[player1] ~= nil) then
-            sendServerMessage(player1, "You are in ft7 mode, you can't duel another bot!")
+            sendServerMessage(player1, "You are in FT7 mode, you cannot duel a bot!")
             return false
         else
             return true
@@ -21,10 +21,10 @@ function checkDuelValid(player1, player2)
     if(ft7_players[player1] == nil and ft7_players[player2] == nil) then
         return true
     elseif(ft7_players[player1] == nil and ft7_players[player2] ~= nil) then
-        sendServerMessage(player1, player2_name .. "is in ft7 mode, duel someone else!")
+        sendServerMessage(player1, player2_name .. "is in FT7 mode, duel someone else!")
         return false
     elseif(ft7_players[player1] ~= nil and ft7_players[player2] == nil) then
-        sendServerMessage(player1, "You are in ft7 mode, you can't duel with non-ft7 players!")
+        sendServerMessage(player1, "You are currently in FT7 mode, you can only duel with players who is also in this mode!")
         return false
     else
         if(ft7_players[player1]["opponent"] == nil and ft7_players[player2]["opponent"] == nil) then
@@ -46,8 +46,8 @@ function refreshScore(winner, loser)
     ft7_players[winner]["score"] = ft7_players[winner]["score"] + 1
     local winner_name = getPlayerName(winner)
     local loser_name = getPlayerName(loser)
-    sendServerMessage(winner, winner_name .. ":" .. ft7_players[winner]["score"] .. " " .. loser_name .. ":" .. ft7_players[loser]["score"] .. ".")
-    sendServerMessage(loser, winner_name .. ":" .. ft7_players[winner]["score"] .. " " .. loser_name .. ":" .. ft7_players[loser]["score"] .. ".")
+    sendServerMessage(winner, winner_name .. ": " .. ft7_players[winner]["score"] .. "  " .. loser_name .. ": " .. ft7_players[loser]["score"] .. ".")
+    sendServerMessage(loser, winner_name .. ": " .. ft7_players[winner]["score"] .. "  " .. loser_name .. ": " .. ft7_players[loser]["score"] .. ".")
     if(ft7_players[winner]["score"] == 7) then
         broadcast(winner_name .. " has won a First to Seven against " .. loser_name .. "! Score: 7-" .. ft7_players[loser]["score"] .. ".")
         ft7_players[winner] = nil
@@ -57,9 +57,8 @@ end
 
 function ft7Check(player)
     if(ft7_players[player] ~= nil) then
-        sendServerMessage(player, "You already entered First to seven mode.")
     else
-        sendServerMessage(player, "You have not entered first to seven mode yet.")
+        sendServerMessage(player, "You have not entered First to Seven mode yet.")
 	return nil
     end
     local opponent = ft7_players[player]["opponent"]
@@ -67,8 +66,8 @@ function ft7Check(player)
         sendServerMessage(player, "You have not started a First to Seven Duel match yet.")
     else
         local opponent_name = getPlayerName(opponent)
-        sendServerMessage(player, "You are currently having a First to Seven Duel match with" .. opponent_name .. ".")
-        sendServerMessage(player, "Your Score: " .. ft7_players[player]["score"] .. ". Your opponent's Score: " .. ft7_players[opponent]["score"] .. ".")
+        sendServerMessage(player, "You are currently having a First to Seven Duel match with " .. opponent_name .. ".")
+        sendServerMessage(player, "Your score: " .. ft7_players[player]["score"] .. ". Your opponent's score: " .. ft7_players[opponent]["score"] .. ".")
     end
 end
 
@@ -81,8 +80,8 @@ function startFt7Relationship(player1, player2)
         ft7_players[player2]["opponent"] = player1
         local player1_name = getPlayerName(player1)
         local player2_name = getPlayerName(player2)
-        sendServerMessage(player1, "Starting First to Seven with" .. player2_name .. ".")
-        sendServerMessage(player2, "Starting First to Seven with" .. player1_name .. ".")
+        sendServerMessage(player1, "Starting First to Seven with " .. player2_name .. ".")
+        sendServerMessage(player2, "Starting First to Seven with " .. player1_name .. ".")
     end
 end
 
@@ -91,9 +90,9 @@ function playerEnterFt7(player)
         ft7_players[player] = {}
         ft7_players[player]["score"] = 0
         ft7_players[player]["opponent"] = nil
-        sendServerMessage(player, "You are ready for a First to Seven duel match, find another FT7 player to duel.")
+        sendServerMessage(player, "You are ready for a First to Seven duel match, find another FT7 player to duel or cancel this by hitting the snowy barrel again.")
     else
-        sendServerMessage(player, "You already entered First to seven mode!")
+        sendServerMessage(player, "You already entered First to Seven mode!")
     end
 end
 
@@ -109,21 +108,21 @@ function playerLeaveFt7(player)
         ft7_players[player] = nil
         sendServerMessage(player, "First to Seven Mode cancelled.")
     else
-        sendServerMessage(player, "You have not entered first to seven mode yet!")
+        sendServerMessage(player, "You have not entered First to Seven mode yet!")
     end
 end
 
 function listFt7(player)
-    sendServerMessage(player, "Players who are in FT7 mode:")
+    sendColoredMessage(player, "[!]: Players looking for FT7 matches:", 0x8CE8FF)
     for ft7_player_id, ft7_player in pairs(ft7_players) do
         ft7_player["visited"] = false
         if(ft7_player["opponent"] == nil) then
             ft7_player["visited"] = true
             local ft7_player_name = getPlayerName(ft7_player_id)
-            sendServerMessage(player, ft7_player_name)
+            sendColoredMessage(player, "     **" .. ft7_player_name .. "**", 0x8CE8FF)
         end
     end
-    sendServerMessage(player, "Ongoing FT7 Duel matches:")
+    sendColoredMessage(player, "[!]: Ongoing FT7 matches:", 0xF28F24)
     for ft7_player_id, ft7_player in pairs(ft7_players) do
         if(ft7_player["visited"] == false) then
             opponent = ft7_player["opponent"]
@@ -131,7 +130,7 @@ function listFt7(player)
             ft7_players[opponent]["visited"] = true
             local player1_name = getPlayerName(ft7_player_id)
             local player2_name = getPlayerName(opponent)
-            sendServerMessage(player, player1_name .. " " .. ft7_players[ft7_player_id]["score"] .. "-" .. ft7_players[opponent]["score"] .. " " .. player2_name )
+            sendColoredMessage(player, "     **" .. player1_name .. " " .. ft7_players[ft7_player_id]["score"] .. "-" .. ft7_players[opponent]["score"] .. " " .. player2_name .. "**", 0xF28F24)
         end
     end
 end
